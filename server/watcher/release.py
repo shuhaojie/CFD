86527baf250ec58205024a6b7750b81253f4f347
shuhaojie <shuhaojie@sina.cn>
2022-12-16 10:35:47 +0800

    feature:和速石对接核心代码

--- server/watcher/release.py
+++ server/watcher/release.py
@@ -1,3 +1,7 @@
+"""
+和速石进行交互的核心代码:
+本脚本会一直对release路径下的文件进行不间断扫描, 有文件过来时会对文件进行判断，如果文件执行完成, 会将文件移入到archive下
+"""
 import os
 import sys
 import time
@@ -8,22 +12,25 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(0, BASE_DIR)
 
 from config import configs  # noqa
-from apps.models import Task, Uknow, Queue  # noqa
+from apps.models import Uknow, IcemTask
 from dbs.database import TORTOISE_ORM  # noqa
-from .utils import get_token  # noqa
+from watcher.utils import get_token  # noqa
 
 
 async def run():
     await Tortoise.init(config=TORTOISE_ORM)
     await Tortoise.generate_schemas()
 
-    query_sets = await Task.filter(status="pending")
+    query_sets = await Uknow.filter(status="pending")
     prepare_path = r"{}".format(configs.PREPARE_PATH)
     if len(query_sets) == 0:
         print(f"No files in {prepare_path} currently.")
     else:
         # 有文件就会要传文件, 先拿token
         token = get_token()
+        headers = {
+            'Authorization': f'Bearer {token}'
+        }
         for query in query_sets:
             # 1. 首先判断文件是否稳定
             task_id = query.task_id
@@ -33,18 +40,42 @@ async def run():
                 pass
             else:
                 # 2. 文件已经稳定, 就可以开始发请求了
-                # 1) 再发申请硬件资源的请求
+                # 1) 上传数据
+                base_url = "http://120.48.150.243/fa/api/v0/upload/"
+                abs_path = os.path.join(r'C:\workspaces\data\prepare', task_id + ".zip")
+                files = {
+                    'file': open(abs_path, 'rb'),
+                    'key': (None, 'dir1/'),
+                    'storage_id': (None, '-1'),
+                }
+                requests.post(
+                    base_url,
+                    files=files,
+                    headers=headers
+                )
+                # 2) 校验文件是否稳定
+                query = await IcemTask.filter(task_id=task_id).first()
+                icem_md5 = query.icem_md5
+                file_complete = False
+                while not file_complete:
+                    base_url = "http://120.48.150.243/fa/api/v0/upload/"
+                    params = {'md5': icem_md5}
+                    r = requests.post(base_url, json=params, headers=headers)
+                    complete = r.json()['complete']
+                    if complete:
+                        file_complete = True
+                    else:
+                        time.sleep(30)
 
-                # 2) 再发数据上传的请求
+                # 3) 再发申请硬件资源的请求
+
                 base_url = 'http://127.0.0.1:8000/reverse_status'
-                header = ''
+                file_path = f'/user/fastone/admin/'
+                params = {'md5': icem_md5, 'config': 'medium', 'file_path': file_path}
                 # https://stackoverflow.com/a/22567429/10844937
-                requests.post(base_url, files=abs_path)
-                # 3) 判断前面两个请求是否成功
-
-                # 4) 成功后发执行脚本的请求
-                pass
-
+                r = requests.post(base_url, json=params, headers=headers)
+                job_id = r.json()['job_id']
+                await IcemTask.filter(task_id=task_id).update(job_id=job_id)
 
 if __name__ == "__main__":
     run_async(run())
