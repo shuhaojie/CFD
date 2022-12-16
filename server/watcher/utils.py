82cc97321e4d7e53b887d7f4d02ce6ac9c20560e
shuhaojie <shuhaojie@sina.cn>
2022-12-16 10:37:41 +0800

    chore:自动控制模块的utils

--- server/watcher/utils.py
+++ server/watcher/utils.py
@@ -1,8 +1,38 @@
+import os
 import requests
+import base64
 
 
 def get_token():
-    base_url = "http://127.0.0.1:8000/login"
-    r = requests.post(base_url)
-    token = r.json()['token']
+    base_url = "http://120.48.150.243/api/v1/auth/signin"
+    password = 'fs@2022!'
+    encoded_password = str(base64.b64encode(password.encode('utf-8')), 'utf-8')
+    params = {'usernameOrEmail': 'admin', 'encodedPassword': encoded_password}
+    r = requests.post(base_url, json=params)
+    token = r.json()['accessToken']
     return token
+
+
+def upload_file(task_id):
+    base_url = "http://120.48.150.243/fa/api/v0/upload/"
+    abs_path = os.path.join(r'C:\workspaces\data\prepare', task_id + ".zip")
+
+    token = get_token()
+    files = {
+        'file': open(abs_path, 'rb'),
+        'key': (None, 'dir1/'),
+        'storage_id': (None, '-1'),
+    }
+    headers = {
+        'Authorization': f'Bearer {token}'
+    }
+    r = requests.post(
+        base_url,
+        files=files,
+        headers=headers
+    )
+    print(r.json())
+
+
+if __name__ == '__main__':
+    upload_file('aaa')
