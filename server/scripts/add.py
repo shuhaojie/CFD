d477f94dcbc36bf2d282df86de866c2787984359
shuhaojie <shuhaojie@sina.cn>
2022-12-27 19:47:07 +0800

    chore:启动脚本新增硬件配置

--- server/scripts/add.py
+++ server/scripts/add.py
@@ -5,7 +5,7 @@ from passlib.context import CryptContext
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(0, BASE_DIR)
-from apps.models import FluentProf, Admin
+from apps.models import FluentProf, Admin, FluentHardware, IcemHardware
 from dbs.database import TORTOISE_ORM
 from config import configs
 
@@ -39,7 +39,7 @@ async def run():
 
     # 2. 入库admin页面
     query = await Admin.all()
-    if len(query) == 2:
+    if len(query) == 1:
         pass
     else:
         username = configs.ADMIN_USERNAME
@@ -49,6 +49,54 @@ async def run():
             username=username,
             password=hash_password,
         )
+
+    # 3. 入库FluentHardware
+    query = await FluentHardware.all()
+    if len(query) == 3:
+        pass
+    else:
+        hardware_list = [
+            {
+                'id': 1,
+                'level': 'low',
+                'fs_instance_type': 'b1.c1.24',
+                'cpu_frequency': 2.6,
+                'cpu_model': 'Intel Xeon Icelake 8350C 32C',
+                'v_cpu': 24,
+                'memory': 48,
+                'price': 6,
+                'system_platform': 'CentOS',
+            },
+            {
+                'id': 2,
+                'level': 'medium',
+                'fs_instance_type': 'b1.c1.32',
+                'cpu_frequency': 2.6,
+                'cpu_model': 'Intel Xeon Icelake 8350C 32C',
+                'v_cpu': 32,
+                'memory': 64,
+                'price': 6,
+                'system_platform': 'CentOS',
+            },
+            {
+                'id': 3,
+                'level': 'high',
+                'fs_instance_type': 'b1.c1.48',
+                'cpu_frequency': 2.6,
+                'cpu_model': 'Intel Xeon Icelake 8350C 32C',
+                'v_cpu': 48,
+                'memory': 96,
+                'price': 6,
+                'system_platform': 'CentOS',
+            },
+        ]
+        for hardware in hardware_list:
+            await FluentHardware.create(
+                **hardware
+            )
+            await IcemHardware.create(
+                **hardware
+            )
 
 
 if __name__ == "__main__":
