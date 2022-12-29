4730ce6cddca75f02b0ce2bd3fc8030ffbd8dafa
shuhaojie <shuhaojie@sina.cn>
2022-12-29 16:14:38 +0800

    bugfix:跑完一个任务后,loop关闭

--- server/worker.py
+++ server/worker.py
@@ -1,3 +1,4 @@
+import asyncio
 from asgiref.sync import async_to_sync
 from celery import Celery
 from apps.task.tasks import monitor_task
@@ -15,4 +16,9 @@ else:
 @celery.task(name="run_task")
 def run_task(task_id):
     celery_task_id = run_task.request.id
-    async_to_sync(monitor_task)(task_id, celery_task_id)
+    # 如果loop关了，要新建一个loop
+    if asyncio.get_event_loop().is_closed():
+        asyncio.new_event_loop()
+        async_to_sync(monitor_task)(task_id, celery_task_id)
+    else:
+        async_to_sync(monitor_task)(task_id, celery_task_id)
