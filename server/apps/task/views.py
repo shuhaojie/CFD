6d03bb047c0847fea51353520749b3543eb23dd4
shuhaojie <shuhaojie@sina.cn>
2022-12-29 10:27:00 +0800

    feature:三个请求合并为一个

--- server/apps/task/views.py
+++ server/apps/task/views.py
@@ -1,60 +1,71 @@
 import os
+import shutil
 import pytz
 import datetime
-from fastapi import UploadFile, BackgroundTasks
+from fastapi import UploadFile
 from fastapi_restful.inferring_router import InferringRouter
+from pydantic.typing import Literal
 from typing import List
 from fastapi import Query
 
-from commons.schemas import BaseResponse
 from apps.models import Uknow
-from .schemas import StartResponseSchema, SendDataRequestSchema
+from .utils import FileTool
+from .schemas import UploadResponse
 from worker import run_task
 from config import configs
 from utils.constant import Status
 
 uknow_router = InferringRouter(prefix="", tags=['UKnow'])
 
-sushi_router = InferringRouter(prefix="", tags=['Sushi'])
 
+# https://stackoverflow.com/a/70657621/10844937
+@uknow_router.post("/upload", name="上传文件", response_model=UploadResponse)
+async def upload(file: UploadFile,
+                 md5: str,
+                 mac_address: str,
+                 prof: Literal['ICA', 'ACA', 'BA', 'MCA', 'VA'],
+                 icem_hardware_level: Literal['low', 'medium', 'high'],
+                 fluent_hardware_level: Literal['low', 'medium', 'high'],
+                 username: str | None = None,
+                 task_name: str | None = None,
+                 ):
+    # 1. 先写入文件
+    try:
+        contents = file.file.read()
+        write_path = os.path.join(configs.MONITOR_PATH, file.filename)
+        with open(write_path, 'wb') as f:
+            f.write(contents)
+    except Exception as e:
+        print(e)
+        return {'code': 200, "message": f"{e}", 'status': False}
+    finally:
+        file.file.close()
 
-@uknow_router.post("/fetch_task_id", name="获取task_id", response_model=StartResponseSchema)
-async def fetch_task_id():
-    now_time = datetime.datetime.now()
-    query = await Uknow.filter(create_time__year=now_time.year,
-                               create_time__month=now_time.month,
-                               create_time__day=now_time.day,
-                               create_time__hour=now_time.hour,
-                               create_time__minute=now_time.minute,
-                               create_time__second=now_time.second)
-    index = len(query) + 1
-    index = f'0{str(index)}' if index < 10 else f'{str(index)}'
-    month = f'0{str(now_time.month)}' if now_time.month < 10 else f'{str(now_time.month)}'
-    day = f'0{str(now_time.day)}' if now_time.day < 10 else f'{str(now_time.day)}'
-    hour = f'0{str(now_time.hour)}' if now_time.hour < 10 else f'{str(now_time.hour)}'
-    minute = f'0{str(now_time.minute)}' if now_time.minute < 10 else f'{str(now_time.minute)}'
-    second = f'0{str(now_time.second)}' if now_time.second < 10 else f'{str(now_time.second)}'
-    task_id = f'{now_time.year}{month}{day}{hour}{minute}{second}0{index}'
-    return {'code': 200, "message": "获取task_id成功", 'status': True, "task_id": task_id}
-
-
-@uknow_router.post("/send_configs", name="发送配置", response_model=BaseResponse)
-async def send_configs(query_data: SendDataRequestSchema):
-    data = query_data.dict()
-    task_id = data.get("task_id", None)
-    username = data.get("username", None)
-    md5 = data.get("md5", None)
-    mac_address = data.get("mac_address", None)
-    task_name = data.get("task_name", None)
-    icem_hardware_level = data.get("icem_hardware_level", None)
-    fluent_hardware_level = data.get("fluent_hardware_level", None)
-    icem_params = data.get("icem_params", None)
-    fluent_params = data.get("fluent_params", None)
-    # 查看当前的数据条数, 如果是条数少于10, is_await就会False, 否则要排队
-    task_id_list = await Uknow.all().values_list("task_id", flat=True)
-    if task_id in task_id_list:
-        return {'code': 200, "message": "task_id已存在", 'status': False}
+    # 2. md5校验
+    file_md5 = FileTool.get_md5(write_path)
+    if file_md5 != md5:
+        return {'code': 200, "message": "md5校验未通过, 请检查数", "task_id": None, "status": False}
+    # 3. 检查文件是否是stl文件
+    elif not file.filename.endswith('stl'):
+        return {'code': 200, "message": "请上传stl文件", "task_id": None, "status": False}
     else:
+        # 4. 生成task_id
+        now_time = datetime.datetime.now()
+        query = await Uknow.filter(create_time__year=now_time.year,
+                                   create_time__month=now_time.month,
+                                   create_time__day=now_time.day,
+                                   create_time__hour=now_time.hour,
+                                   create_time__minute=now_time.minute,
+                                   create_time__second=now_time.second)
+        index = len(query) + 1
+        index = f'0{str(index)}' if index < 10 else f'{str(index)}'
+        month = f'0{str(now_time.month)}' if now_time.month < 10 else f'{str(now_time.month)}'
+        day = f'0{str(now_time.day)}' if now_time.day < 10 else f'{str(now_time.day)}'
+        hour = f'0{str(now_time.hour)}' if now_time.hour < 10 else f'{str(now_time.hour)}'
+        minute = f'0{str(now_time.minute)}' if now_time.minute < 10 else f'{str(now_time.minute)}'
+        second = f'0{str(now_time.second)}' if now_time.second < 10 else f'{str(now_time.second)}'
+        task_id = f'{now_time.year}{month}{day}{hour}{minute}{second}0{index}'
+        # 5. 数据入库
         await Uknow.create(
             task_id=task_id,
             md5=md5,
@@ -63,41 +74,18 @@ async def send_configs(query_data: SendDataRequestSchema):
             task_name=task_name,
             icem_hardware_level=icem_hardware_level,
             fluent_hardware_level=fluent_hardware_level,
-            icem_params=icem_params,
-            fluent_params=fluent_params,
+            fluent_prof=prof,
         )
-        return {'code': 200, "message": "配置发送成功", 'status': True}
-
-
-# https://stackoverflow.com/a/70657621/10844937
-@uknow_router.post("/upload_file", name="上传文件", response_model=BaseResponse)
-async def upload(file: UploadFile):
-    if not os.path.exists(configs.MONITOR_PATH):
-        os.makedirs(configs.MONITOR_PATH)
-    if not os.path.exists(configs.PREPARE_PATH):
-        os.makedirs(configs.PREPARE_PATH)
-    if not os.path.exists(configs.ARCHIVE_PATH):
-        os.makedirs(configs.ARCHIVE_PATH)
-    try:
-        contents = file.file.read()
-        write_path = os.path.join(configs.MONITOR_PATH, file.filename)
-        with open(write_path, 'wb') as f:
-            f.write(contents)
-    except Exception as e:
-        return {"message": f"There was an error {e} uploading the file"}
-    finally:
-        file.file.close()
-    task_id = file.filename.split('.')[0]
-    task_id_list = await Uknow.all().values_list("task_id", flat=True)
-    if task_id not in task_id_list:
-        return {'code': 200, "message": "stl文件名错误", 'status': False}
-    else:
+        # 6. 对文件重命名
+        standard_file = os.path.join(configs.MONITOR_PATH, task_id + '.stl')
+        shutil.move(write_path, standard_file)
+        # 7. 发送异步任务
         run_task.apply_async((task_id,))
-        return {'code': 200, "message": "文件上传成功", 'status': True}
+        return {'code': 200, "message": "文件上传成功", 'task_id': task_id, 'status': True}
 
 
-@uknow_router.get("/reverse_status", name="轮询任务状态")
-async def reverse_status(task_id_list: List[str] = Query([], title="task_id列表")):
+@uknow_router.get("/get_status", name="状态查询")
+async def get_status(task_id_list: List[str] = Query([], title="task_id列表")):
     all_task_id_list = await Uknow.all().values_list("task_id", flat=True)
     if len(task_id_list) == 0:
         return {'code': 200, "message": "请输入task_id", 'status': False}
