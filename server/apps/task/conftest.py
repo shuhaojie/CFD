50b591a8730243b07ad785aa3d897bf1d3a474b3
shuhaojie <shuhaojie@sina.cn>
2023-02-09 14:08:03 +0800

    bugfix:smtp无法连接

--- server/apps/task/conftest.py
+++ server/apps/task/conftest.py
@@ -58,4 +58,35 @@ def send_mail(task_status='SUCCESS'):
     smtp.quit()
 
 
-send_mail()
+def new_send_email():
+    # skipped your comments for readability
+    import smtplib
+    from email.mime.multipart import MIMEMultipart
+    from email.mime.text import MIMEText
+
+    me = BUSINESS.EMAIL_FROM
+    my_password = BUSINESS.EMAIL_PASSWORD
+    you = "shuhaojie@unionstrongtech.com"
+
+    msg = MIMEMultipart('alternative')
+    msg['Subject'] = "Alert"
+    msg['From'] = me
+    msg['To'] = you
+
+    html = '<html><body><p>Hi, I have the following alerts for you!</p></body></html>'
+    part2 = MIMEText(html, 'html')
+
+    msg.attach(part2)
+
+    # Send the message via gmail's regular server, over SSL - passwords are being sent, afterall
+    s = smtplib.SMTP_SSL(BUSINESS.EMAIL_HOST)
+    # uncomment if interested in the actual smtp conversation
+    # s.set_debuglevel(1)
+    # do the smtp auth; sends ehlo if it hasn't been sent already
+    s.login(me, my_password)
+
+    s.sendmail(me, you, msg.as_string())
+    s.quit()
+
+
+new_send_email()
