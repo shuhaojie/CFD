ed340729f4c20a6165337eedb5f75bf78fa67e57
shuhaojie <shuhaojie@sina.cn>
2023-02-14 11:27:20 +0800

    chore:任务名称不允许重复

--- server/apps/task/utils.py
+++ server/apps/task/utils.py
@@ -405,7 +405,7 @@ def download_complete(file_path):
 def create_job(task_id, task_type, md5, headers, hardware_level='middle', solver='3ddp', parallel_number=28):
     base_url = 'http://120.48.150.243/api/v1/jobs'
     now = datetime.now()
-    task_name = f'{now.year}{now.month}{now.day}{now.hour}{now.minute}{now.second}_{task_type}'
+    task_name = f'{now.year}{now.month}{now.day}{now.hour}{now.minute}{now.second}_{task_type}_{configs.ENVIRONMENT}'
     if task_type == 'icem':
         # 目前暂时用这个固定配置, 这个是根据用户选择的配置来的
         if hardware_level == 'middle':
