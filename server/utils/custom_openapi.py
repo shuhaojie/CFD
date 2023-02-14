fccae8c1da92d6138319f511a8392e3932f764a1
shuhaojie <shuhaojie@sina.cn>
2023-02-14 10:03:45 +0800

    chore:修改文档title

--- server/utils/custom_openapi.py
+++ server/utils/custom_openapi.py
@@ -1,4 +1,5 @@
 from fastapi.openapi.utils import get_openapi
+from config import configs
 
 
 def init_openapi(app):
@@ -8,8 +9,14 @@ def init_openapi(app):
         """去除422响应"""
         if app.openapi_schema:
             return app.openapi_schema
+        if configs.ENVIRONMENT == 'development':
+            title = "CFD自动化平台api-研发环境"
+        elif configs.ENVIRONMENT == 'production':
+            title = "CFD自动化平台api-正式环境"
+        else:
+            title = "CFD自动化平台api-测试环境"
         openapi_schema = get_openapi(
-            title="CFD自动化平台api",
+            title=title,
             description="模块接口文档",
             version="1.0.0",
             routes=app.routes,
