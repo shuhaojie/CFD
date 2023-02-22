3f5d7d2b4fd4333328ca1655f8eac4bbdca0a497
shuhaojie <shuhaojie@sina.cn>
2023-02-22 10:29:38 +0800

    chore:排查worker是否可能有bug

--- server/worker.py
+++ server/worker.py
@@ -1,8 +1,10 @@
 import asyncio
+import traceback
 from asgiref.sync import async_to_sync
 from celery import Celery
 from apps.task.tasks import monitor_task
 from config import configs
+from logs import api_log
 
 celery = Celery("CFD")
 if configs.ENVIRONMENT == 'local':
@@ -15,14 +17,15 @@ else:
 
 @celery.task(name="run_task")
 def run_task(task_id):
-    celery_task_id = run_task.request.id
-    loop = asyncio.get_event_loop()
-    future = loop.create_task(monitor_task(task_id, celery_task_id))
-    loop.run_until_complete(asyncio.wait([future]))
-    future.result()
-    # # 如果loop关了，要新建一个loop
-    # if asyncio.get_event_loop().is_closed():
-    #     asyncio.new_event_loop()
-    #     async_to_sync(monitor_task)(task_id, celery_task_id)
-    # else:
-    #     async_to_sync(monitor_task)(task_id, celery_task_id)
+    try:
+        celery_task_id = run_task.request.id
+        loop = asyncio.get_event_loop()
+        future = loop.create_task(monitor_task(task_id, celery_task_id))
+        loop.run_until_complete(asyncio.wait([future]))
+        future.result()
+    except Exception as e:
+        f = traceback.format_exc()
+        api_log.info(e)
+        api_log.info(f)
+        print(e)
+        print(f)
