c55b8ab67bd5e4d5bf5ceee08ec130499303a693
shuhaojie <shuhaojie@sina.cn>
2023-02-28 11:41:00 +0800

    bigfix:background task会阻塞

--- server/worker.py
+++ server/worker.py
@@ -23,6 +23,7 @@ celery.conf.broker_heartbeat = 0
 def run_task(task_id):
     try:
         loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
         future = loop.create_task(monitor_task(task_id))
         loop.run_until_complete(asyncio.wait([future]))
         future.result()
